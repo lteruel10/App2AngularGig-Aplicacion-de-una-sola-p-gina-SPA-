a83b17bf4e7b4e7973db08b7841b835844a69e9f
lteruel10 <lteruel10@gmail.com>
2021-07-27 09:54:59 -0600

    60. Rutas con parametros - Router

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService,Heroe } from '../../services/heroes.services';
-
+import{Router} from '@angular/router'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,15 +10,20 @@ import { HeroesService,Heroe } from '../../services/heroes.services';
 export class HeroesComponent implements OnInit {
 
   heroes:Heroe[]=[];
-  constructor(private _heroesService:HeroesService) { 
-    console.log("Bob");
+  constructor(private _heroesService:HeroesService,
+              private _router:Router//el lo puso como router:Router
+              ) { 
+    // console.log("Bob");
   }
 
   ngOnInit() {
     console.log('ngOnInit');
     this.heroes=this._heroesService.getHeroes();
-    console.log(this.heroes)
+    // console.log(this.heroes)
 
   }
+  verHeroe(idx:number){
+    this._router.navigate(['/heroe',idx])  
+  }//verHeroe
 
 }
